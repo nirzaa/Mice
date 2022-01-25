8a9718d352d3bc363ff689d7c3380092f8340b97
nirzaa <72485675+nirzaa@users.noreply.github.com>
2022-01-25 11:59:56 +0200

    updated

--- src/mice/main/box_menu_ising.py
+++ src/mice/main/box_menu_ising.py
@@ -113,8 +113,8 @@ def ising_box_runner(idx, T, max_epochs, batch_size, freq_print, genom, lr, weig
 
 def ising_temp():
     Ts = [round(T,2) for T in np.linspace(0.1, 4, 5)]
-    # Ts = [0.1, 1, 2, 2.1, 2.2, 2.3, 2.4, 2.7, 2.9, 3, 3.2, 4]
-    Ts = [2.1]
+    Ts = [0.1, 1, 2, 2.1, 2.2, 2.3, 2.4, 2.7, 2.9, 3, 3.2, 4]
+
     mi = np.zeros(len(Ts))
 
     for idx, T in enumerate(Ts):
